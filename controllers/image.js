f992d6738254d6c627fde6ef9acfb391343200b8
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-06-03 13:23:43 +0600

    api key changed

--- controllers/image.js
+++ controllers/image.js
@@ -1,38 +1,40 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 const app = new Clarifai.App({
-  apiKey: 'b5624aafd619411f8accfc04e257cd73'
+  apiKey: "b5624aafd619411f8accfc04e257cd73",
 });
 
 const apiRequest = (req, res) => {
-  app.models.predict("a403429f2ddf4b49b307e318f00e528b", req.body.imageUrl)
-  .then(data => res.json(data),
-    // the introduced link is not a proper one
-    (err) => {
-        res.json('badLink');
-    }
-  )
-  // image does not contain faces
-  .catch(err => {
-    res.json('error');
-  });   
-}
+  app.models
+    .predict("f462cceec4324ad69e878059a3d9f8b0", req.body.imageUrl)
+    .then(
+      (data) => res.json(data),
+      // the introduced link is not a proper one
+      (err) => {
+        res.json("badLink");
+      }
+    )
+    // image does not contain faces
+    .catch((err) => {
+      res.json("error");
+    });
+};
 
 const handleImage = (req, res, knex) => {
-	knex('users')
-  	.where({ 
-  		id: Number(req.body.id) 
-  	})
-  	.increment('entries', 1)
-  	.then(result => {
-  		if (result === 0) {
-  			res.json('fail');
-  		} else {
-  			res.json('success');
-  		}
-  	});
-}
+  knex("users")
+    .where({
+      id: Number(req.body.id),
+    })
+    .increment("entries", 1)
+    .then((result) => {
+      if (result === 0) {
+        res.json("fail");
+      } else {
+        res.json("success");
+      }
+    });
+};
 
 module.exports = {
-	handleImage: handleImage,
-  apiRequest: apiRequest
-}
+  handleImage: handleImage,
+  apiRequest: apiRequest,
+};
