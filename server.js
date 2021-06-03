7cae253fe9b0e11e1bc8955e2bbcdbf3d3760676
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-06-03 15:47:26 +0600

    1

--- server.js
+++ server.js
@@ -17,13 +17,9 @@ const knex = require("knex")({
 
 const app = express();
 // middleware
+app.header("Access-Control-Allow-Origin", "*");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 // just a greet message
 app.get("/", (req, res) => {
