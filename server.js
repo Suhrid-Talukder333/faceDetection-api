8b29cb2bc3ddc87e3d2565cc384a169352532cdc
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-06-11 10:38:56 +0600

    change

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
-const signin = require("./controllers/signin.js");
-const register = require("./controllers/register.js");
-const profile = require("./controllers/profile.js");
-const image = require("./controllers/image.js");
-const { check, validationResult } = require("express-validator/check");
-const knex = require("knex")({
+const knex = require("knex");
+
+const app = express();
+
+const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
@@ -15,50 +14,90 @@ const knex = require("knex")({
   },
 });
 
-const app = express();
-// middleware
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.json());
 
-// just a greet message
 app.get("/", (req, res) => {
-  res.send("It is working!");
-});
-// signin endpoint
-app.post(
-  "/signin",
-  [
-    check("email").isEmail().normalizeEmail(),
-    check("password").isLength({ min: 3 }).escape(),
-  ],
-  (req, res) => {
-    signin.handleSignIn(req, res, knex, bcrypt, validationResult);
-  }
-);
-// register endpoint
-app.post(
-  "/register",
-  [
-    check("name").isLength({ min: 2 }).trim().escape(),
-    check("email").isEmail().normalizeEmail(),
-    check("password").isLength({ min: 3 }).escape(),
-  ],
-  (req, res) => {
-    register.handleRegister(req, res, knex, bcrypt, validationResult);
-  }
-);
-// get user profile
-app.get("/profile/:id", (req, res) => {
-  profile.handleProfile(req, res, knex);
-});
-// api request
-app.post("/apiRequest", (req, res) => {
-  image.apiRequest(req, res);
-});
-// update entries
-app.put("/image", (req, res) => {
-  image.handleImage(req, res, knex);
+  res.send("hello there");
 });
 
-app.listen(process.env.PORT || 3000);
+app.post("/signin", (req, res) => {
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is running");
+});
